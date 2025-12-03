d2dc47742bc8ca85f6c0ef04598594fa5f5efbd6
HP <HP@Sinchana>
2025-12-03 17:31:45 +0530

    My first commit

---
+++ Projectformavengithubjenkinkspractice/src/test/java/package1/Class1Test.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1Test {
+
+	@Test
+	public void demo() {
+		Reporter.log("************hiii**********",true);
+
+	}
+}
---
+++ Projectformavengithubjenkinkspractice/src/test/java/package1/Class3Test.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class3Test {
+
+	@Test
+	public void demo() {
+		Reporter.log("************hiii**********",true);
+
+	}
+}
